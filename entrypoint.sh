a77ac89a703b6252c1168b3b8652ce524cc39400
yahya <yahyaest1000@gmail.com>
2024-04-04 20:42:37 +0100

    Implement Production Mode

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/bin/sh
+ENV=${ENV:-PROD}
+
 
 # Create or update .env file with provided environment variables
 
@@ -9,8 +11,16 @@ echo "JWT_SECRET=${JWT_SECRET}" >> /app/.env
 echo "CLIENT_URL=${CLIENT_URL}" >> /app/.env
 echo "CLIENT_DOMAIN=${CLIENT_DOMAIN}" >> /app/.env
 
+# Change to the app directory
+cd /app
 
 # Continue with the CMD (your main application command)
 # exec "$@"
 
-npm run start:migrate:dev
+if [ "$ENV" = "PROD" ]
+then
+    npm run build
+    npm run start:migrate:prod
+else
+    npm run start:migrate:dev
+fi
